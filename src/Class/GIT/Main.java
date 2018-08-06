a95d064211f2cf294626469e33115496faf4f7bb
Xan <xan_namazov@mail.ru>
2018-08-07 00:42:11 +0400

    Third

--- src/Class/GIT/Main.java
+++ src/Class/GIT/Main.java
@@ -3,7 +3,6 @@ package Class.GIT;
 public class Main {
     public static void main(String[] args) {
         System.out.println("MAIN git");
-
-
+        System.out.println("Nothing Changed");
     }
 }
